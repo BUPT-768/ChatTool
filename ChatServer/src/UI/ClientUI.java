e83ae01b1e8047690b9d63993dc5db6e87260932
Jayvee_He <jayveehe@gmail.com>
2014-12-15 11:48:16 +0800

    添加UI包（正在编辑）

---
+++ ChatServer/src/UI/ClientUI.java
@@ -0,0 +1,15 @@
+package UI;
+
+import javax.swing.*;
+
+/**
+ * Created by ITTC-Jayvee on 2014/12/15.
+ */
+public class ClientUI {
+    private JButton btn_connect;
+    private JTextField text_port;
+    private JTextField text_addr;
+    private JTextPane textScreen;
+    private JTextField textInput;
+    private JButton 发送Button;
+}
